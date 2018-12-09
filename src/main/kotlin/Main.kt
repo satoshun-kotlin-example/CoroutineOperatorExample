cabc1e2b3390a79f39783b7b0d971ce6eb436867
SatoShun <shun.sato1@gmail.com>
2018-12-09 10:00:21 +0000

    some sampels

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,33 @@
-fun main(args: Array<String>) {
-  println(args)
+import kotlinx.coroutines.delay
+
+suspend fun main(args: Array<String>) {
+  fakeCompletable()
+
+  val a = fakeSingle()
+  println(a)
+
+  val b = fakeMaybe()
+  println(b)
+}
+
+suspend fun fakeCompletable() {
+  delay(3000)
+}
+
+suspend fun fakeSingle(): Int {
+  delay(3000)
+  return 10
+}
+
+suspend fun fakeMaybe(): Int? {
+  delay(3000)
+  return 10
+}
+
+suspend fun sampleResult() {
+  val r = runCatching {
+    delay(1000)
+    100
+  }
+  r.map { }
 }
